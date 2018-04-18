f9d270cddd1b8d39b8d080055e8a027e5f13584a
sahityadronamraju <36168371+sahityadronamraju@users.noreply.github.com>
2018-04-18 09:43:38 +0530

    Update Hello.java

--- JAVA/src/Hello.java
+++ JAVA/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO EVERYONE");
+		System.out.println("HELLO WORLD");
 		
 
 	}
